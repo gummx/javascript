2e74cdcf14758455c311026c6e783458059fbf6b
Miguel <mgm425@hotmail.com>
2015-10-20 19:23:48 -0500

    Ejercicios Javascript

---
+++ js/ciclos.js
@@ -0,0 +1,72 @@
+/*var i;
+for (i=0; i <= 10; i++) {
+	console.log("i vale: " + i);
+	if (i==3) {
+		break
+	};
+};
+console.log("El ciclo for terminó.");
+*/
+/*
+var i=0;
+
+do{
+	console.log("i vale: " + i);
+	i++
+} while(i<=10);	
+console.log("El ciclo do-while terminó.");
+sandbox,
+memory leak
+OCR
+reconocimiento optico de caracteres
+*/
+/*
+var i=1;
+while(i<=10){
+	i++;	
+	if (i==3) {
+		continue;
+	}
+	console.log("i vale: " + i);
+}
+console.log("El ciclo do-while terminó.");
+*/
+
+
+
+
+
+
+var total=0;
+
+for(var i=0; i<=100;i++)
+{
+	if(i%2==0){
+		 total = total + i;
+		//total += even;
+		//console.log(even)
+	}	
+}
+console.log("El resultado de la sumatoria es: " + total)
+
+/**/
+/*
+var factorial=1, n=1;
+
+for(n; n<=4;n++)
+{
+		factorial *= n;
+}
+console.log(factorial)
+*/
+/*
+var array=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var index = array.length;
+var x=index-1;
+for(x; x>=0 ;x--){
+			console.log(array[x])
+}
+var array2 = array.reverse()
+console.log(array2)
+
+*/
---
+++ js/juego.js
@@ -0,0 +1,88 @@
+// Single line comment
+
+//alert("Hola mundo"+" "+"Adiós" + (new Date().toString()));
+//
+/**
+	Comentario largo de Donald Knut
+	Literate programming, propósito del método y valores de los parámetros
+
+*/
+
+console.log("Hola")
+
+var cuatro = "4";
+var cinco = 5;
+
+ if (cuatro == cinco) {
+	console.log("cuatro es igual cinco"); 	
+ }
+else {
+	console.log("cuatro NO es igual a cinco");
+ };	
+
+var tijeras ="tijeras", papel="papel", piedra="piedra";
+var jugador ="papel";
+console.log("Jugador: " + jugador)
+
+
+if (persona == "piedra") {
+	if (compu ="papel"){
+
+		console.log("Gana compu");
+	}
+	if (compu == "tijeras") {
+		console.log("Gana persona");
+
+	}
+	if (compu == "piedra"){
+
+		console.log("Empate");
+	}
+}
+if (persona == "papel") {
+	if (compu == "papel"){
+
+		console.log("Empate");
+	}
+	if (compu == "tijeras") {
+		console.log("Pierde persona");
+
+	}
+	if (compu == "piedra"){
+
+		console.log("Gana Persona");
+	}
+
+}
+if (persona == "piedra") {
+	if (compu == "papel"){
+
+		console.log("Gana compu");
+	}
+	if (compu == "tijeras") {
+		console.log("Gana persona");
+
+	}
+	if (compu == "piedra"){
+
+		console.log("Empate");
+	}
+}
+
+/*
+
+switch(jugador){
+	case tijeras:
+		console.log("JS: Piedra!");
+		break;
+	case piedra:
+		console.log("JS: Papel!");
+		break
+	//case papel:
+	default:
+		console.log("JS: Tijeras!");
+		break;
+}
+*/
+
+
